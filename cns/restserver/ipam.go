18cf1f8e14e06c8dfb4e596474d717927997af94
Evan Baker <rbtr@users.noreply.github.com>
2021-06-05 17:44:21 -0500

    AllocateAnyAvailableIPConfig

--- cns/restserver/ipam.go
+++ cns/restserver/ipam.go
@@ -483,28 +483,29 @@ func (service *HTTPRestService) AllocateDesiredIPConfig(podInfo cns.PodInfo, des
 }
 
 func (service *HTTPRestService) AllocateAnyAvailableIPConfig(podInfo cns.PodInfo) (cns.PodIpInfo, error) {
-	var podIpInfo cns.PodIpInfo
 
 	service.Lock()
 	defer service.Unlock()
 
+	var podIpInfo cns.PodIpInfo
 	for _, ipState := range service.PodIPConfigState {
-		if ipState.State == cns.Available {
-			_, err := service.setIPConfigAsAllocated(ipState, podInfo)
-			if err != nil {
-				return podIpInfo, err
-			}
-
-			err = service.populateIpConfigInfoUntransacted(ipState, &podIpInfo)
-			if err != nil {
-				return podIpInfo, err
-			}
-
+		if ipState.State != cns.Available {
+			continue
+		}
+		_, err := service.setIPConfigAsAllocated(ipState, podInfo)
+		if err != nil {
 			return podIpInfo, err
 		}
+
+		err = service.populateIpConfigInfoUntransacted(ipState, &podIpInfo)
+		if err != nil {
+			return podIpInfo, err
+		}
+
+		return podIpInfo, err
 	}
 
-	return podIpInfo, fmt.Errorf("No more free IP's available, waiting on Azure CNS to allocated more IP's...")
+	return podIpInfo, fmt.Errorf("no more free IPs available, waiting on Azure CNS to allocated more IP's...")
 }
 
 // If IPConfig is already allocated for pod, it returns that else it returns one of the available ipconfigs.
